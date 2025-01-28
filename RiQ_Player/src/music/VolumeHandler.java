d52cb5ecd67ea5377e57902fda3c8ffe12c0e97e
Dev-RiQ <lwk525678@gmail.com>
2025-01-28 19:25:52 +0900

    feat : create volume control line (button -> drag)

--- RiQ_Player/src/music/VolumeHandler.java
+++ RiQ_Player/src/music/VolumeHandler.java
@@ -1,10 +1,19 @@
 package music;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.sound.sampled.FloatControl;
+import javax.swing.JLabel;
 
-public class VolumeHandler{
+import _main.Panel;
 
-	
+public class VolumeHandler implements MouseListener, MouseMotionListener{
+
+	private JLabel label;
 	private FloatControl gain;
 	private FloatControl setting;
 	
@@ -15,7 +24,35 @@ public class VolumeHandler{
 	}
 
 	VolumeHandler() {
-		setGain((float) -15.0);
+		setGain((float) -14.0);
+	}
+	
+	/** create volume controller */
+	public void addVolumeCont(Panel panel) {
+		label = new JLabel("Vol-   ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡●=======   Vol+");
+		label.setForeground(Color.WHITE);
+		label.setPreferredSize(new Dimension(340,25));
+		label.setVerticalAlignment(JLabel.TOP);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.addMouseListener(this);
+		label.addMouseMotionListener(this);
+		panel.add(label);
+	}
+	
+	/** draw volume line per (total play time / 25) */
+	public void playTimeLine(int idx) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Vol-   ");
+		for(int i = 0 ; i < 25 ; i++) {
+			if(i < idx)
+				sb.append("≡");
+			else if ( i == idx)
+				sb.append("●");
+			else
+				sb.append("=");
+		}
+		sb.append("   Vol+");
+		label.setText(sb.toString());
 	}
 
 	/** set volume gain */
@@ -29,18 +66,44 @@ public class VolumeHandler{
 	public void volumeSetting() {
 		setGain((float) setting.getValue());
 	}
-
-	/** volume down */
-	public void volumeDown() {
-		float dB = (float) (setting.getValue() - 1.0);
-		if(dB < (float) -80.0) dB = (float) -80.0;
-		setGain(dB);
+	
+	// X : 80 ~ 260
+	// gain : 0 ~ -50
+	/** volume control by move volume bar */
+	private void setVolume(MouseEvent e) {
+		int x = e.getX();
+		if(e.getX() < 80) x = 80;
+		if(e.getX() > 260) x = 260;
+		int idx = (int) ((x-80)*1.0 / 180 * 24);
+		float volumeMove = (float) (-50.0 + (x-80)*1.0 / 179 * 50);
+		if(volumeMove < -48)
+			setGain((float) -80.0);
+		else
+			setGain(volumeMove);
+		playTimeLine(idx);
 	}
 
-	/** volume up */
-	public void volumeUp() {
-		float dB = (float) (setting.getValue() + 1.0);
-		if(dB > (float) 0.0) dB = (float) 0.0;
-		setGain(dB);
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		setVolume(e);
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		setVolume(e);
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
 	}
 }
