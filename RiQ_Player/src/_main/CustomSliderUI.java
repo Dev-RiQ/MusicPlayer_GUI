1fb7e0fb19189c2324ddd63ba5582a17090c3067
Dev-RiQ <lwk525678@gmail.com>
2025-02-04 16:40:02 +0900

    refactor : delete stroke (non-use)

--- RiQ_Player/src/_main/CustomSliderUI.java
+++ RiQ_Player/src/_main/CustomSliderUI.java
@@ -1,21 +1,16 @@
 package _main;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.Stroke;
 import javax.swing.JComponent;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 import javax.swing.plaf.basic.BasicSliderUI;
 
 public class CustomSliderUI extends BasicSliderUI {
-
-    private BasicStroke stroke = new BasicStroke(1f, BasicStroke.CAP_ROUND, 
-            BasicStroke.JOIN_ROUND, 0f, new float[]{1f, 2f}, 0f);
 
     int y;
     public CustomSliderUI(JSlider b, int y) {
@@ -40,15 +35,12 @@ public class CustomSliderUI extends BasicSliderUI {
     public void paintTrack(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         int x1 = thumbRect.x + 5;
-        Stroke old = g2d.getStroke();
-        g2d.setStroke(stroke);
         g2d.setPaint(Color.GRAY);
         g2d.fillRoundRect(0, 3, x1,5,5,5);
         if (slider.getOrientation() == SwingConstants.HORIZONTAL) {
         	g2d.setPaint(Color.LIGHT_GRAY);
             g2d.fillRoundRect(x1, 3, y - x1,5,5,5);
         }
-        g2d.setStroke(old);
     }
 
     @Override
@@ -60,8 +52,5 @@ public class CustomSliderUI extends BasicSliderUI {
         g2d.fillRoundRect(x1, 0, 11,11,100,100);
         g2d.setPaint(Color.DARK_GRAY);
         g2d.fillRoundRect(x2, 1, 9,9,100,100);
-        Stroke old = g2d.getStroke();
-        g2d.setStroke(new BasicStroke(2f));
-        g2d.setStroke(old);
     }
 }
